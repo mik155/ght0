4fd85ca52b32e10e75d0adf31b3618958559ae36
Nicolaus <mik155@wp.pl>
2024-12-22 20:37:22 +0100

    a

--- .github/workflows/actions/deploy-via-js/main.js
+++ .github/workflows/actions/deploy-via-js/main.js
@@ -3,7 +3,12 @@ const core = require('@actions/core')
 const exec = require('@actions/exec')
 
 function run() {
+    const bucket = core.getInput('bucket', { required: true })
+    const region = core.getInput('region', { required: true })
+    const folder = core.getInput('folder', { required: true })
+
     core.notice('Hellow form js action !')
+    core.notice(`bucket, region, folder: ${bucket}, ${region}, ${folder}`)
 }
 
 run();
