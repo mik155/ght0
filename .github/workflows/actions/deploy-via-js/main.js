03b513d1c3dc5f0fd22d16e0ad433b8d911ab726
Nicolaus <mik155@wp.pl>
2024-12-22 20:04:56 +0100

    a

--- .github/workflows/actions/deploy-via-js/main.js
+++ .github/workflows/actions/deploy-via-js/main.js
@@ -1,6 +1,6 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
-const exec = require('@action/exec')
+const exec = require('@actions/exec')
 
 function run() {
     core.notice('Hellow form js action !')
